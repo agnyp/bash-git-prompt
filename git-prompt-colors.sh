fc0a5d6d13be01274de8c1bba3f5703278f1369a
Andreas Gnyp <andreas@gnyp.de>
2019-04-25 10:43:20 +0200

    More space for some icons

--- git-prompt-colors.sh
+++ git-prompt-colors.sh
@@ -7,9 +7,9 @@ GIT_PROMPT_SUFFIX=" ]"                 # the end of the git info string
 GIT_PROMPT_SEPARATOR="|"              # separates each item
 
 GIT_PROMPT_BRANCH="${Magenta}"        # the git branch that is active in the current directory
-GIT_PROMPT_STAGED="${Green} ●"           # the number of staged files/directories
-GIT_PROMPT_CONFLICTS=" ${BackRed}✖"        # the number of files in conflict
-GIT_PROMPT_CHANGED="${Red} ✚"         # the number of changed files
+GIT_PROMPT_STAGED="${Green} ● "           # the number of staged files/directories
+GIT_PROMPT_CONFLICTS=" ${BackRed}✖ "        # the number of files in conflict
+GIT_PROMPT_CHANGED="${Red} ✚ "         # the number of changed files
 GIT_PROMPT_REMOTE=" "                 # the remote branch name (if any) and the symbols for ahead and behind
 GIT_PROMPT_UNTRACKED="${Cyan} …"       # the number of untracked files/dirs
 GIT_PROMPT_STASHED="${BoldBlue} ⚑"     # the number of stashed files/dir
